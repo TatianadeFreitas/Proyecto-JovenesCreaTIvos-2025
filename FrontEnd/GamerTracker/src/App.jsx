0dc4fe624c45f8ea82fa381ea4864d8c40d5f786
TatianadeFreitas <maitetorrespipo@gmail.com>
2025-10-14 14:15:02 -0300

    Inicio de la App

---
+++ FrontEnd/GamerTracker/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Hola Bienvendo a GamerTracker</h1>
+
+      <a href='index.jsx'><button>Iniciar Experiencia</button></a>
+    </>
+  )
+}
+
+export default App
